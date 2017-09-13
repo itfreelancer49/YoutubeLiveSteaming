2ecb10f462c7a2de2ecec6479032bb7e8f8b6822
Noman Khan <nomankhan21@gmail.com>
2017-09-13 19:55:45 +0530

    initial commit

---
+++ app/src/main/java/phonetubestreaming/google/android/apps/watchme/PermissionsActivity.java
@@ -0,0 +1,133 @@
+package phonetubestreaming.google.android.apps.watchme;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Mohammad Arshi Khan on 13-09-2017.
+ */
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 45;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setPermission();
+    }
+
+    private void setPermission() {
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, android.Manifest.permission.INTERNET))
+            permissionsNeeded.add("Internet");
+        if (!addPermission(permissionsList, android.Manifest.permission.ACCESS_NETWORK_STATE))
+            permissionsNeeded.add("NETWORK_STATE");
+        if (!addPermission(permissionsList, android.Manifest.permission.GET_ACCOUNTS))
+            permissionsNeeded.add("GET_ACCOUNTS");
+        if (!addPermission(permissionsList, android.Manifest.permission.CAMERA))
+            permissionsNeeded.add("Camera");
+
+
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "You will need to grant permissions for Live Streaming.";
+//                String message = "You need to grant access to " + permissionsNeeded.get(0);
+//                for (int i = 1; i < permissionsNeeded.size(); i++)
+//                    message = message + ", " + permissionsNeeded.get(i);
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(PermissionsActivity.this, permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            ActivityCompat.requestPermissions(this, permissionsList.toArray(new String[permissionsList.size()]),
+                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            return;
+        }
+
+        startNextActivity();
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+
+        new AlertDialog.Builder(PermissionsActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permission))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+            {
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(android.Manifest.permission.INTERNET, PackageManager.PERMISSION_GRANTED);
+                perms.put(android.Manifest.permission.ACCESS_NETWORK_STATE, PackageManager.PERMISSION_GRANTED);
+                perms.put(android.Manifest.permission.GET_ACCOUNTS, PackageManager.PERMISSION_GRANTED);
+                perms.put(android.Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION
+                if (perms.get(android.Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(android.Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(android.Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+                        ) {
+                    // All Permissions Granted
+                    startNextActivity();
+
+                } else {
+                    // Permission Denied
+                    Toast.makeText(PermissionsActivity.this, "Some Permission is Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void startNextActivity(){
+        startActivity(new Intent(PermissionsActivity.this, MainActivity.class));
+    }
+}
